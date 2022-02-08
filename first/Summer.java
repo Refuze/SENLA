34ff05f22d6ae3ec567157c82a14e57cb5d2bc6a
Refuze <refuzeapple@gmail.com>
2022-02-08 10:32:22 +0300

    Summary
    First task updated

--- first/Summer.java
+++ first/Summer.java
@@ -1,16 +1,22 @@
 package first;
 import java.util.Scanner;
 
+/*Напишите программу, которая возвращает сумму цифр,
+присутствующих в данной строке. Если цифр нет, то возвращаемая
+сумма должна быть равна 0*/
+
 public class Summer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
         int sum = 0;
-        for (int i = 0; i < string.length(); i++) {
-            if (Character.isDigit(string.charAt(i))){
-                sum += string.charAt(i) - '0';
+
+        for (char symbol : string.toCharArray()) {
+            if (Character.isDigit(symbol)){
+                sum += symbol - '0';
             }
         }
+
         System.out.println(sum);
     }
 }
